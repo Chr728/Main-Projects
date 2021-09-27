bcbd72d1a805052b55d5ae3952533efc9265b627
Christina Darstbanian <minasderstep@gmail.com>
2021-09-26 22:56:54 -0400

    Delete FillMood/backend directory

--- FillMood/backend/api/interfacesSpotify.go
+++
@@ -1,6 +0,0 @@
-package api
-
-type InterfaceSpotify interface {
-	Authorization(clientId, clientSecret string)	error
-	SearchPlaylist(params string) PlaylistModel
-}
--- FillMood/backend/endpointsControllers/request.go
+++
@@ -1,9 +0,0 @@
-package endpointsControllers
-
-
-type MoodParams struct {
-	Mood		string	`json:"mood" validate:"required"`
-	Fitevent	string	`json:"fitevent" validate:"required"`
-	Playlist	string	`json:"playlist" validate:"required"`
-}
-
