b486938951da08bd680392cbe8381b472eaceadf
Christina Darstbanian <minasderstep@gmail.com>
2021-11-12 19:16:28 -0500

    Update MyArrayList.java

--- Data Structures and Algorithm/Array and Linked List/MyArrayList.java
+++ Data Structures and Algorithm/Array and Linked List/MyArrayList.java
@@ -3,8 +3,8 @@
  * Assignment 2
  * Summer 2021
  * 
- * James Partsafas 40170301
- * Christina Darstbanian 40097340
+ * James Partsafas 
+ * Christina Darstbanian
  */
 
 public class MyArrayList<E> implements List<E> {
