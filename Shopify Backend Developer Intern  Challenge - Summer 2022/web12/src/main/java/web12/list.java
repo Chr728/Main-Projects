3e6953a8357338cb9437b87be427525803885534
Christina Darstbanian <minasderstep@gmail.com>
2022-01-11 01:20:33 -0500

    Update list.java

--- Shopify Backend Developer Intern  Challenge - Summer 2022/web12/src/main/java/web12/list.java
+++ Shopify Backend Developer Intern  Challenge - Summer 2022/web12/src/main/java/web12/list.java
@@ -1,5 +1,7 @@
 package web12;
-
+/*
+Author: Christina Darstbanian
+*/
 import java.util.ArrayList;
 
 import java.io.File;
@@ -8,12 +10,19 @@ import java.util.ArrayList;
 
 public class list {
 	static int counter = 0;
+	// all the items are stored in an ArrayList instead of using a database
 	public static ArrayList<item> items = new ArrayList<>();
-
+         
+	/*
+	the function adds items to the list by having the quantity and the name of the item
+	*/
 	public static void insertitem(int quantity, String name) {
 		items.add(new item(counter++, name, quantity));
 	}
 
+	/*
+	the function deletes items from the list using the unique item id of each item.
+	*/
 	public static void deletetitem(int id) {
 		int index = -1;
 		for (int i = 0; i < items.size(); i++) {
@@ -26,6 +35,11 @@ public class list {
 			items.remove(index);
 		}
 	}
+	
+	/*
+	the function updates the quantity and name of a specific item.
+	Note  that the id is unique for each item and I chose to not change any specific id.
+	*/
 
 	public static void updateitem(int id, int quantity, String name) {
 		int index = -1;
@@ -41,6 +55,11 @@ public class list {
 		}
 
 	}
+	
+	
+	/*
+	extra feature where by clicking it will generate a .csv file on the desktop
+	*/
 	
 	public static void exportCSV() {
 		StringBuilder stringBuilder = new StringBuilder();
@@ -64,147 +83,3 @@ public class list {
 
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//public class list {
-//	static int counter=1;
-//	public static ArrayList<item>  items= new ArrayList<>();
-//	
-//
-//	
-//	public static void insertitem(int quantity, String name)
-//	{
-//		
-//		items.add(new item(counter++,name,quantity));
-//		
-//	}
-//	
-//	public static void deletetitem(int id)
-//	{
-//		System.out.println(id);
-//	   int index=-1;
-//	   for ( int i=0; i < items.size(); i++) 
-//	   {
-//		 if(items.get(i).getId() == id) 
-//		 {
-//		   index=i; 
-//		   break;	 
-//			 
-//		 }
-//		   
-//	   }
-//	   if(index  != -1) {
-//	   items.remove(index);
-//	   }
-//	}
-//	
-//	
-//	
-//	
-//	
-//	public static void updateitem(int id, int quantity, String name) {
-//		 int index=-1;
-//		   for ( int i=0; i < items.size(); i++) 
-//		   {
-//			 if(items.get(i).getId() == id) 
-//			 {
-//			   index=i; 
-//			   break;	 
-//				 
-//			 }
-//			   
-//		   }
-//		
-//		   if(index  != -1) {
-//			   items.get(index).setquantity(quantity);
-//			   items.get(index).setName(name);
-//			   }
-//		
-//	}
-//	
-//}
-//
-//
